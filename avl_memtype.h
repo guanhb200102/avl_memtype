e968ff9a91cbe9ab2e892cb34df6c4e6fd39f524
guanhb200102 <18938763920@163.com>
2025-03-10 09:56:56 +0800

    Update avl_memtype.h

--- avl_memtype.h
+++ avl_memtype.h
@@ -14,8 +14,24 @@ typedef struct AVLmemtype_node {
 } AVLmemtype_node;
 
 //user api fun:
+
+//func: insert_AVLmemtype(AVLmemtype_node** ppself, void* psrc_data, int siz)
+//return:
+//	0: normal
+//	-1: value conflied
+//	-2: memery fail
 int insert_AVLmemtype(AVLmemtype_node**, void*, int siz);
+
+//func: delete_AVLmemtype
+//return:
+//	-1: value not present
+//	0: work
 int delete_AVLmemtype(AVLmemtype_node**, void*, int siz);
+
+//func: search_AVLmemtype
+//return:
+//	0: cant found
+//	!0: work
 AVLmemtype_node* search_AVLmemtype(AVLmemtype_node*, void*, int siz);
 
 
